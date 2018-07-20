595618524ad62ecd87607704bc7ffc192dfbccef
Busche, Matthew <m.busche@nationwide.com>
2018-07-19 22:32:52 -0500

    update endpoint for user vehicle

--- src/main/java/com/nationwide/totalrecall/services/VehicleRecallService.java
+++ src/main/java/com/nationwide/totalrecall/services/VehicleRecallService.java
@@ -1,6 +1,7 @@
 package com.nationwide.totalrecall.services;
 
 import com.nationwide.totalrecall.domain.VehicleRecalls;
+import com.nationwide.totalrecall.repository.IUserVehicleRecallRepository;
 import com.nationwide.totalrecall.repository.IVehicleRecallRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,13 +12,23 @@ import java.util.List;
 public class VehicleRecallService {
 
 	private IVehicleRecallRepository vehicleRecallRepository;
+	private IUserVehicleRecallRepository userVehicleRecallRepository;
 
 	@Autowired
 	public void setVehicleRecallRepository(IVehicleRecallRepository vehicleRecallRepository) {
 		this.vehicleRecallRepository = vehicleRecallRepository;
 	}
 
+	@Autowired
+	public void setUserVehicleRecallRepository(IUserVehicleRecallRepository userVehicleRecallRepository) {
+		this.userVehicleRecallRepository = userVehicleRecallRepository;
+	}
+
 	public List<VehicleRecalls> getRecallsByPolicyNumber(String policyNumber) {
 		return vehicleRecallRepository.findAllByPolicyNumber(policyNumber);
+	}
+
+	public void updateUserVehicleRecallById(Integer vehicleRecallId, Integer statusId) {
+		userVehicleRecallRepository.updateUserVehicleRecallById(vehicleRecallId, statusId);
 	}
 }
