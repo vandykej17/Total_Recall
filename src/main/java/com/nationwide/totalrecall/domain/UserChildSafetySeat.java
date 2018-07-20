dd9fb45de796642cd63c23f7a02f2ca48bdda18b
Busche, Matthew <m.busche@nationwide.com>
2018-07-19 20:36:08 -0500

    fix alex

--- src/main/java/com/nationwide/totalrecall/domain/UserChildSafetySeat.java
+++ src/main/java/com/nationwide/totalrecall/domain/UserChildSafetySeat.java
@@ -1,44 +1,44 @@
-package com.nationwide.totalrecall.domain;
-
-import javax.persistence.*;
-
-@Entity
-@Table(name = "User_Child_Safety_Seat")
-public class UserChildSafetySeat {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer id;
-	private Users user;
-	private  ChildSafetySeat childSafetySeat;
-
-	@ManyToOne
-	@JoinColumn(name = "User_Id")
-	public Users getUser() {
-		return user;
-	}
-
-	public void setUser(Users user) {
-		this.user = user;
-	}
-
-	@ManyToOne
-	@JoinColumn(name = "Child_Safety_Seat_Id")
-	public ChildSafetySeat getChildSafetySeat() {
-		return childSafetySeat;
-	}
-
-	public void setChildSafetySeat(ChildSafetySeat childSafetySeat) {
-		this.childSafetySeat = childSafetySeat;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public UserChildSafetySeat() {
-	}
-}
+//package com.nationwide.totalrecall.domain;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "User_Child_Safety_Seat")
+//public class UserChildSafetySeat {
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+//	private Integer id;
+//	private Users user;
+//	private  ChildSafetySeat childSafetySeat;
+//
+//	@ManyToOne
+//	@JoinColumn(name = "User_Id")
+//	public Users getUser() {
+//		return user;
+//	}
+//
+//	public void setUser(Users user) {
+//		this.user = user;
+//	}
+//
+//	@ManyToOne
+//	@JoinColumn(name = "Child_Safety_Seat_Id")
+//	public ChildSafetySeat getChildSafetySeat() {
+//		return childSafetySeat;
+//	}
+//
+//	public void setChildSafetySeat(ChildSafetySeat childSafetySeat) {
+//		this.childSafetySeat = childSafetySeat;
+//	}
+//
+//	public Integer getId() {
+//		return id;
+//	}
+//
+//	public void setId(Integer id) {
+//		this.id = id;
+//	}
+//
+//	public UserChildSafetySeat() {
+//	}
+//}
